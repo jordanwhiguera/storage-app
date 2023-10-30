1232114f39f8e3a6b6d49e135b6dd48d19ae7f5c
Jordan Higuera <jordanwhiguera@gmail.com>
2023-10-29 20:29:20 -0400

    file structureand daisyUI

---
+++ app/screens/Home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <div>
+      Home Screen
+      <button className="btn w-64 rounded-full">Button</button>
+    </div>
+  );
+};
+
+export default HomeScreen;
