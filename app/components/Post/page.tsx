4483b8aff7beb48ba0c142cd149eaaa147c081bf
Jordan Higuera <jordanwhiguera@gmail.com>
2023-11-03 17:30:46 -0400

    dummy data and dynamic data in posts

--- app/components/Post/page.tsx
+++ app/components/Post/page.tsx
@@ -2,33 +2,35 @@ import React from "react";
 import { AiFillStar } from "react-icons/ai";
 import { FaTruckPickup } from "react-icons/fa";
 import { GrLocation } from "react-icons/gr";
+import feed from "@/app/assets/data/feed";
 
 const Post = () => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
-        <img
-          src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Storage"
-        />
+        <img src={feed[0].image} alt="Storage Image" />
       </figure>
       <div className="card-body">
         <div className="flex justify-between items-center">
-          <h2 className="card-title">Storage Description</h2>
+          <h2 className="card-title truncate">{feed[0].type}</h2>
+          {/* Use "truncate" to cut off the text with an ellipsis */}
           <div className="flex items-center">
-            <AiFillStar className="text-yellow-500" /> 5 (13)
+            <AiFillStar className="text-yellow-500" /> {feed[0].stars} (
+            {feed[0].numReviews})
           </div>
         </div>
-        <p>Storage Title</p>
+        <p className="truncate">{feed[0].title}</p>
         <p className="flex items-center">
-          <GrLocation className="mr-2" /> 2 miles away
+          <GrLocation className="mr-2" /> {feed[0].distance} miles away
         </p>
-        <p>$100 monthly before taxes</p>
+        <p>${feed[0].price} monthly before taxes</p>
         <div className="card-actions justify-start">
           <div className="badge badge-secondary">
-            <FaTruckPickup className="mr-1" /> Courier+
+            <FaTruckPickup className="mr-1" /> {feed[0].carType}
           </div>
-          <div className="badge badge-secondary">$40 per delivery</div>
+          <div className="badge badge-secondary">
+            ${feed[0].deliveryPrice} per delivery
+          </div>
         </div>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import HomeScreen from "./screens/Home/page";
 import Post from "./components/Post/page";
+import feed from "./assets/data/feed";
 export default function Base() {
   return (
     <>
       {/* <HomeScreen /> */}
+      <Post />
+      <Post />
       <Post />
     </>
   );
