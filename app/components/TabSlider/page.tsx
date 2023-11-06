679b96ffce705ea34e54e0692ad6f6c0455cc878
Jordan Higuera <jordanwhiguera@gmail.com>
2023-11-05 19:06:09 -0500

    TabSlider scroll completed

---
+++ app/components/TabSlider/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import React, { useRef } from "react";
+import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai";
+
+const TabSlider = () => {
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  // Scroll the container by a certain amount to the left
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({
+        left: -300, // Adjust the value as needed
+        behavior: "smooth",
+      });
+    }
+  };
+
+  // Scroll the container by a certain amount to the right
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({
+        left: 300, // Adjust the value as needed
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <div className="bg-black w-full flex items-center mt-8 p-4 relative">
+      {/* Left scroll icon */}
+      <button onClick={scrollLeft} className="mr-2">
+        <AiFillLeftCircle size={30} color="white" />
+      </button>
+
+      {/* Container for the buttons with a max-width and overflow-hidden */}
+      <div
+        ref={scrollContainerRef}
+        className="flex flex-nowrap overflow-x-hidden"
+      >
+        <button className="btn mr-2">Delivery</button>
+        <button className="btn mr-2">Self-Storage</button>
+        <button className="btn mr-2">Vehicle Storage</button>
+        <button className="btn mr-2">Airport Parking</button>
+        {/* Right scroll icon */}
+      </div>
+      <button onClick={scrollRight} className=" mr-2">
+        <AiFillRightCircle size={30} color="white" />
+      </button>
+
+      {/* Filter button */}
+      <button className="btn">Filter</button>
+    </div>
+  );
+};
+export default TabSlider;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Post from "./components/Post/page";
 import SearchResults from "./screens/SearchResults/page";
 import Navbar from "./components/Navbar/page";
 import SearchButton from "./components/SearchButton/page";
+import TabSlider from "./components/TabSlider/page";
 export default function Base() {
   return (
     <>
@@ -11,7 +12,8 @@ export default function Base() {
       {/* <Post /> */}
       {/* <Post /> */}
 
-      <Navbar />
+      {/* <Navbar /> */}
+      <TabSlider />
       {/* <SearchResults /> */}
       {/* <SearchButton /> */}
     </>
