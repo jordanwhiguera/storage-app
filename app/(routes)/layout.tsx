8b2de4a4f0417e06a1ccdd04f0808f8f7cf40e89
Jordan Higuera <jordanwhiguera@gmail.com>
2023-12-01 16:00:22 -0500

    added new layout .tsx

---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,4 @@
+const NewLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className=" bg-red-600"> {children}</div>;
+};
+export default NewLayout;
---
+++ app/(routes)/yeet/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const yeet = () => {
+  return <div>yeet</div>;
+};
+
+export default yeet;
